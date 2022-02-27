5d45d32dcf737963b8d82f31c2cffe54f61ca499
PamelaAlvigeze <paavigeze@gmail.com>
2022-02-26 22:40:15 -0300

    Cria novas rotas, melhora nomes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from "react";
-import Cadastro from "./components/Cliente/Cadastro";
-import ListaClientes from "./components/Cliente/ListaClientes";
 import Header from "./components/Header";
-import CadastroVideo from "./components/Video/Cadastro";
-import ListaVideos from "./components/Video/ListaVideos";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import CadastraOuEditaCliente from "./pages/CadastraOuEditaCliente";
+import ListaClientes from "./pages/ListaClientes";
+import CadastraOuEditaVideo from "./pages/CadastraOuEditaVideo";
+import ListaVideos from "./pages/ListaVideos";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="container my-3 d-flex justify-content-center">
       <BrowserRouter>
         <Routes>
-          <Route path="/cadastrarClientes" element={<Cadastro />} />
-          <Route path="/listarClientes" element={<ListaClientes />} />
-          <Route path="/cadastrarVideos" element={<CadastroVideo />} />
-          <Route path="/listarVideos" element={<ListaVideos />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/cadastrarCliente" element={<CadastraOuEditaCliente />} />
+          <Route path="/editarCliente/:id" element={<CadastraOuEditaCliente />} />
+          <Route path="/listaDeClientes" element={<ListaClientes />} />
+          <Route path="/cadastrarVideo" element={<CadastraOuEditaVideo />} />
+          <Route path="/editarVideo/:id" element={<CadastraOuEditaVideo />} />
+          <Route path="/listaDeVideos" element={<ListaVideos />} />
         </Routes>
       </BrowserRouter>
+      </div>
     </div>
   );
 }
